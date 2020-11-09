4b1aee2070bb4e5a61e06551553f20cf0f0af847
ratchet34 <44235571+ratchet34@users.noreply.github.com>
2020-11-09 18:09:35 +0100

    add put and getItemByType

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const get = require('./get');
+const put = require('./put');
 
 const cors = require('cors');
 
@@ -12,11 +13,41 @@ router.get('/test', (req, res) => {
   res.send('Hello World!');
 });
 
-router.get('/item/:itemId', async (req, res) => {
+router.get('/item/id/:itemId', async (req, res) => {
   const item = await get.getItem(req.params.itemId);
   if (!item) return res.status(404).json({});
 
   return res.json(item.body);
+});
+
+router.get('/item/type/:itemType', async (req, res) => {
+  const item = await get.getItem(req.params.itemType);
+  if (!item) return res.status(404).json({});
+
+  return res.json(item.body);
+});
+
+router.post('/item', async (req, res) => {
+  const item = {
+    id: req.body.id,
+    type: req.body.type,
+    time: req.body.time,
+    answerTime: req.body.answerTime,
+    from: req.body.from
+  };
+
+  if ('title' in req.body) {
+    item.title = req.body.title;
+  }
+  if ('artist' in req.body) {
+    item.artist = req.body.artist;
+  }
+  if ('origin' in req.body) {
+    item.origin = req.body.origin;
+  }
+
+  const result = await put.putItem(item);
+  return res.json(result);
 });
 
 app.use('/', router);
--- get.js
+++ get.js
@@ -1,4 +1,3 @@
-const { dynamoDocumentClient } = require('./helper/dynamo.js');
 const han = require('./libs/handler-lib');
 const dynamoDb = require('./libs/dynamodb-lib');
 
@@ -11,8 +10,6 @@ const getItem = han.handler(async (itemId) => {
     }
   };
 
-  console.log('running getItem');
-
   const result = await dynamoDb.get(params);
 
   if (!result.Item) {
@@ -20,9 +17,35 @@ const getItem = han.handler(async (itemId) => {
   } else {
     console.log(`Item found : ${result.Item}`);
   }
-  console.log('after await get');
   // Return the retrieved item
   return result.Item;
 });
 
-module.exports = { getItem };
+const getItemByType = han.handler(async (itemType) => {
+  const params = {
+    TableName: 'quizz-o-tron-items',
+    // 'Key' defines the partition key and sort key of the item to be retrieved
+    // 'KeyConditionExpression' defines the condition for the query
+    // - 'userId = :userId': only return items with matching 'userId'
+    //   partition key
+    // 'ExpressionAttributeValues' defines the value in the condition
+    // - ':userId': defines 'userId' to be Identity Pool identity id
+    //   of the authenticated user
+    KeyConditionExpression: 'type = :itemType',
+    ExpressionAttributeValues: {
+      ':itemType': itemType
+    }
+  };
+
+  const result = await dynamoDb.query(params);
+
+  if (!result.Items) {
+    throw new Error('Item not found.');
+  } else {
+    console.log(`Items found : ${result.Items}`);
+  }
+  // Return the retrieved item
+  return result.Items;
+});
+
+module.exports = { getItem, getItemByType };
